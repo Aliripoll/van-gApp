18a53215f17b48fedd09d686e54cb5c8a885e63b
Aliripoll <aripollortega@gmail.com>
2023-05-12 13:18:42 +0200

    add new component

---
+++ src/components/FilterByText.js
@@ -0,0 +1,17 @@
+
+function FilterByText() {
+    return (
+        <div className="form_div">
+        <label className=""> Search: 
+            <input 
+             type="text" 
+             id="name" 
+             value="name" 
+             placeholder="the starry night" >
+            </input>
+        </label>
+        </div>
+    )
+}
+
+export { FilterByText };
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FilterByTechnique } from "./FilterByTechnique";
+import { FilterByText } from "./FilterByText";
 
 
 
@@ -16,6 +17,7 @@ function Filters( {inputTechnique, handleTechniqueInput} ) {
                 inputTechnique={inputTechnique} 
                 handleTechniqueInput={handleTechniqueInput}
             />
+            <FilterByText />
         </form>
     )
 };
