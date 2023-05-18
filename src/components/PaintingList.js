37e0c44f3f726e9914fd350fd91a3f2893b3ac51
Aliripoll <aripollortega@gmail.com>
2023-05-18 17:36:50 +0200

    add search message

--- src/components/PaintingList.js
+++ src/components/PaintingList.js
@@ -7,14 +7,22 @@ function PaintingList({ dataPainting }) {
     const paintingList = dataPainting.map((painting) => {
         return <PaintingCard key={painting.id} painting={painting} />;
     })
-    return (
-        <>
-            <ul className="cards">
-                {paintingList}
-            </ul>
-          
-        </>
-    )
+console.log(dataPainting.length);
+    if (dataPainting.length !== 0) {
+        return (
+            <>
+                <ul className="cards">
+                    {paintingList}
+                </ul>
+              
+            </>
+        )
+    } else {
+        return (
+        <p className="error">search not found</p>
+        )
+    }
+ 
 }
 
 export { PaintingList };
